a88f2bfe2b130d017755e6561aa15ef471e9c577
smallo92 <smallo92@gmail.com>
2020-11-21 08:01:32 +1000

    Update config.lua
    change to use vectors instead of a table that has to be slowly unpacked

--- config.lua
+++ config.lua
@@ -8,13 +8,10 @@ config = {
 	-- "name" = The name of the particle you want. --
 	-- "scale" = The scale of the particle you want. (Default = 1.0) --
 	-- "coords" = Contains the x,y,z, coordinates for the particle. --
-	-- "x" = The "X" coordinate of the particle. --
-	-- "y" = The "Y" coordinate of the particle. --
-	-- "z" = The "Z" coordinate of the particle. --
 
 	particles = {
-		{dict = 'core', name = 'ent_amb_smoke_chicken', scale = 1.0, coords = {x = -2020.85, y = -1038.95, z = 1.00}},
-		{dict = 'core', name = 'ent_amb_smoke_foundry', scale = 1.0, coords = {x = -2069.53, y = -1023.06, z = 10.00}}
+		{dict = 'core', name = 'ent_amb_smoke_chicken', scale = 1.0, coords = vec(-2020.85, -1038.95, 1.00)},
+		{dict = 'core', name = 'ent_amb_smoke_foundry', scale = 1.0, coords = vec(-2069.53, -1023.06, 10.00)}
 	}
 
 }
